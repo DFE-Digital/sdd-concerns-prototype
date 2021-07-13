91b124607500c4922a7c5b3aa5b4853d9b4a62b5
dfe-steveoc <Steve.OCONNOR@education.gov.uk>
2021-07-13 16:11:39 +0100

    Adding routing to case setup

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ router.get('/concerns/outgoing-trust-search', (req, res) => {
   if (req.query['outgoing-trust-id']) {
     req.session.data['outgoing-trust-id'] = req.query['outgoing-trust-id']
     req.session.data['autocompleted-outgoing-trust-id'] = true
-    res.redirect('/concerns/trust-details')
+    res.redirect('/concerns/trust-details-one')
   } else {
     res.render('concerns/outgoing-trust-search')
   }
@@ -24,23 +24,56 @@ router.get('/concerns/incoming-trust-search', (req, res) => {
 })
 
 
-// Branching
-router.post('/concerns/elements/create-case-answer', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
+// Case Setup Routes //
 
-  const concern = req.session.data['concern-type']
+// match the page here to form action //
+router.post('/trust-details-one', function (req, res) {
 
-  if (concern === 'Finance') {
-    res.redirect('/concerns/elements/finance')
+  // Make a variable and give it the value of your radio group's 'name'
+  var projectStatus = req.session.data['case-type']
+
+  // Check whether the variable matches a condition - your radio values 
+  if (projectStatus == "log"){
+  res.redirect('concerns/trust-details-log')
+  } else if (projectStatus == "concern"){
+  res.redirect('concerns/trust-details-concern')
+  } else if (projectStatus == "safeguarding"){
+  res.redirect('concerns/trust-details-safeguarding')
+  } else if (projectStatus == "srma"){
+  res.redirect('concerns/trust-details-srma')
   }
-  if (concern === 'Governance') {
-    res.redirect('/concerns/elements/governance')
-  } else {
-    res.redirect('/concerns/elements/irregularity')
-  }
-})
+
+  })
+
+router.post('/trust-details-concern', function (req, res) {
+    
+  // Make a variable and give it the value of your radio group's 'name'
+    var projectStatus = req.session.data['concernType-1']
+
+    // Check whether the variable matches a condition - your radio values 
+    if (projectStatus == "compliance-financial-reporting"){
+    res.redirect('concerns/compliance-financial-reporting')
+    } else if (projectStatus == "compliance-financial-returns"){
+    res.redirect('concerns/compliance-financial-returns')
+    }
+  })
+
+/*
+router.post('/trust-details-concern', function (req, res) {
+     // Make a variable and give it the value of your radio group's 'name'
+     var projectStatus = req.session.data['concernType-2']
+     // Check whether the variable matches a condition - your radio values
+    } else if (projectStatus == "financial-deficit"){
+    res.redirect('concerns/financial-deficit')
+    } else if (projectStatus == "financial-projected-deficit"){
+    res.redirect('concerns/financial-projected-deficit')
+    } else if (projectStatus == "financial-cash-flow-shortfall"){
+    res.redirect('concerns/financial-cash-flow-shortfall')
+    } else if (projectStatus == "financial-clawback"){
+    res.redirect('concerns/financial-clawback')
+    }
+  })
+*/
 
 
 module.exports = router
